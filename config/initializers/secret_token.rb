c7d817e4d75ac916449e8b7cb00a2564b7fa17c8
Fernando Paladini <fnpaladini@gmail.com>
2013-11-27 23:01:06 -0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BtcStores::Application.config.secret_key_base = '62c2e29bf599c020d09b18daf8a3a344af23776848cadafb882c7c5ea52f6074e6983dec8fdb12566fc3fba82497a7fa32e00b0a6db01321ee7496d01b64661d'
