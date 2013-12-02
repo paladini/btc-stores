aaf1fad1d9ca889feb75dc71758885bd6fad7ab4
Fernando Paladini <fnpaladini@gmail.com>
2013-12-01 22:43:59 -0200

    Added index for categories

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,13 +1,17 @@
 class StaticController < ApplicationController
   def index
+  	@render_header = true
   end
 
   def about
+  	@render_header = true
   end
 
   def contact
+  	@render_header = true
   end
 
   def resume
+  	@render_header = false
   end
 end
