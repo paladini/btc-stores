5fba2ced20d9bd9733e604a129aa0bf18a141aaf
Fernando Paladini <fnpaladini@gmail.com>
2013-12-14 00:40:03 -0200

    A lot of changes: 1-Finished search 2-Finished_create_new_store_page 3-Validations 4-sort_by_name_asc_in_new_store_page etc

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,5 +1,9 @@
 class Country < ActiveRecord::Base
 
+	# Relationships
 	has_many :item
+
+	# Validations
+	validates_uniqueness_of :name, :case_sensitive => false
 	
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -3,9 +3,26 @@ class Item < ActiveRecord::Base
 	# Relationship with Country
 	belongs_to :country
 	accepts_nested_attributes_for :country
+	# validates_associated :country - this model already is inserted, don't need to check the 
+	# validaty of the model, because this is already validated - just need check if this field is 
+	# filled in. 
 
 	# Relationship with Category
 	belongs_to :category 
 	accepts_nested_attributes_for :category
+	# validates_associated :category
+
+	# General validations
+	validates :name, length: { minimum: 3, maximum: 48 }, presence: true, 
+	uniqueness: true
+
+	validates :description, length: { minimum: 5, maximum: 140 }, presence: true
+
+	validates :url_original, length: { minimum: 8, maximum: 70 }, presence: true, 
+	uniqueness: true
+
+	validates :country_id, presence: true
+	validates :category_id, presence: true
+
 
 end
